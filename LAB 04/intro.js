88c80005a7c84d13bc8281913bc3a362ec3230e1
Oscar-AVS <alexandervilchis638@gmail.com>
2026-02-13 23:31:02 -0600

    Lab 03 terminado

---
+++ LAB 04/intro.js
@@ -0,0 +1,6 @@
+// consola (log, info, warn, error, assert)
+console.log("Hola mundo");
+console.info("Esto es una información");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error");
+console.assert(1 === 2, "Esto no es cierto");
