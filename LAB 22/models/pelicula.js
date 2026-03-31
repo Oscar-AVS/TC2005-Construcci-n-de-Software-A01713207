95ff60bb98c71e2da3db5d1ccf978b4a34bd052f
Oscar-AVS <alexandervilchis638@gmail.com>
2026-03-30 22:02:30 -0600

    Se empieza lab 22

---
+++ LAB 22/models/pelicula.js
@@ -0,0 +1,35 @@
+// Lab 22 - A01713207 Oscar Alexander Vilchis Soto
+// models/pelicula.js
+const db = require('../util/database');
+
+module.exports = class Pelicula {
+
+  constructor(titulo, imagen, sinopsis) {
+    this.titulo   = titulo;
+    this.imagen   = imagen;
+    this.sinopsis = sinopsis;
+  }
+
+  save() {
+    return db.execute(
+      'INSERT INTO peliculas (titulo, imagen, sinopsis) VALUES (?, ?, ?)',
+      [this.titulo, this.imagen, this.sinopsis]
+    );
+  }
+
+  static fetchAll() {
+    return db.execute('SELECT * FROM peliculas');
+  }
+
+  static fetchOne(id) {
+    return db.execute('SELECT * FROM peliculas WHERE id = ?', [id]);
+  }
+
+  static updateById(id, titulo, imagen, sinopsis) {
+    return db.execute(
+      'UPDATE peliculas SET titulo = ?, imagen = ?, sinopsis = ? WHERE id = ?',
+      [titulo, imagen, sinopsis, id]
+    );
+  }
+
+};
---
+++ LAB 22/models/user.js
@@ -0,0 +1,36 @@
+// Lab 22 - A01713207 Oscar Alexander Vilchis Soto
+// models/user.js
+const db     = require('../util/database');
+const bcrypt = require('bcrypt');
+
+module.exports = class User {
+
+    constructor(mi_username, mi_password, mi_nombre) {
+        this.username = mi_username;
+        this.password = mi_password;
+        this.nombre   = mi_nombre;
+    }
+
+    save() {
+        return bcrypt.hash(this.password, 12).then((password_cifrado) => {
+            return db.execute("INSERT INTO users(username, password, nombre) VALUES (?,?,?)",
+                [this.username, password_cifrado, this.nombre]
+            );
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    static fetchOne(username) {
+        return db.execute("SELECT * FROM users WHERE username=?", [username]);
+    }
+
+    static getPrivilegios(username) {
+        return db.execute(
+            `SELECT privilegio FROM tiene t, roles r, otorga o, privilegios p
+            WHERE id_usuario=? AND t.id_rol=r.id AND r.id=o.id_rol AND id_privilegio=p.id`,
+            [username]
+        );
+    }
+
+}
